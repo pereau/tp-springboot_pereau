f8f0362bd709a808af49aeac3a49366d4594412f
pereau <alban.pereau@yahoo.fr>
2017-12-17 23:22:22 +0100

    affichage des noms des adversaires

--- src/main/java/com/igs/ipi/tpspringbootPereauAlban/Controller/GameController.java
+++ src/main/java/com/igs/ipi/tpspringbootPereauAlban/Controller/GameController.java
@@ -1,5 +1,7 @@
 package com.igs.ipi.tpspringbootPereauAlban.Controller;
 
+import com.igs.ipi.tpspringbootPereauAlban.Model.GameModel;
+import com.igs.ipi.tpspringbootPereauAlban.Service.GameService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,6 +13,12 @@ public class GameController {
     @RequestMapping("/game/new")
     public ModelAndView newGame(){
         ModelAndView mav = new ModelAndView("game"); //Instanciation d'un objet de type ModelAndView qui s'affiche dans game (.html)
+
+        GameService gameService = new GameService();
+
+        mav.addObject("nom1", gameService.newGame().getNom1());
+        mav.addObject("nom2", gameService.newGame().getNom2());
+
 
         return mav;
     }
---
+++ src/main/java/com/igs/ipi/tpspringbootPereauAlban/Service/GameService.java
@@ -0,0 +1,22 @@
+package com.igs.ipi.tpspringbootPereauAlban.Service;
+
+import com.igs.ipi.tpspringbootPereauAlban.Model.GameModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+
+
+
+    public GameModel newGame(){
+
+        GameModel gameModel = new GameModel();
+
+        gameModel.setNom1("joueur1");
+        gameModel.setNom2("joueur2");
+
+        return gameModel;
+
+    }
+
+}
